21f53737efa8ee03f1a51d8aef72fa868f195c02
11Aravind <aravindsreenandanam11@gmail.com>
2023-12-26 14:40:07 +0530

    frond end address management was created

--- GreenNestClient/src/pages/Manageaddress.js
+++ GreenNestClient/src/pages/Manageaddress.js
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,useReducer } from "react";
 import { getAddress } from "./API";
 import { httpRequest } from "../API/api";
+import { useDispatch, useSelector } from "react-redux";
 import "./Order.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Manageaddress = () => {
   const [address, setAddress] = useState([]);
@@ -12,11 +13,15 @@ const Manageaddress = () => {
     });
   }, []);
   const setAddressCallBack = (data) => {
+    console.log(address);
     setAddress(data);
   };
   const [viewForm,setForm]=useState(false)
   // console.log(viewForm)
   // console.log(address)
+  const changeviewForm=()=>{
+    setForm(!viewForm);
+  }
   return (
     <>
       {/* <h1>Manageaddress page</h1> */}
@@ -26,11 +31,11 @@ const Manageaddress = () => {
           <div className="order_id font12" onClick={()=>setForm(!viewForm)}>
             <b>
           
-           {viewForm?( <span> Cancel</span>  ):(<span> <i className="fa fa-plus" aria-hidden="true" ></i>ADD A NEW ADDRESS</span> )}
+           {viewForm?( ""  ):(<span className="pointer"> <i className="fa fa-plus" aria-hidden="true" ></i>ADD A NEW ADDRESS</span> )}
 
             </b>
           </div>
-          {viewForm?<Addaddress />: ""}
+          {viewForm?<Addaddress  changeviewForm={changeviewForm} setAddressCallBack={setAddressCallBack}/>: ""}
         </div> 
         </div>
       {address.map((oneAddress, index) => {
@@ -75,7 +80,9 @@ export const Oneaddress = ({ addressObj, setAddressCallBack }) => {
       <b>Total:</b> 
     </div> */}
             <div className="transaction_id font12">
-              <b>{address}</b>
+              <b><textarea name="" value={address.replace(/_/g, " , ")} id="editAddressBox" cols="50" rows="5" readOnly>
+              
+                </textarea></b>
             </div>
             <div className="status font12" >
               <i className="fa fa-pencil-square-o"
@@ -105,8 +112,49 @@ export const Oneaddress = ({ addressObj, setAddressCallBack }) => {
 
 
 
-export const Addaddress=()=>{
-
+export const Addaddress=({changeviewForm,setAddressCallBack})=>{
+  
+const initialAddress={
+  address:"",
+  locality:"",
+  // landmark:"",
+  city:"",
+  // state:"",
+  pincode:"",
+  altphoneno:"",
+}
+const user_id = useSelector((state) => state.user.user_id);
+const addressReducer = (addressState, action) => {
+  switch (action.type) {
+    case "UPDATE_INPUT":
+      return { ...addressState, [action.payload.name]: action.payload.value };
+    default:
+      return addressState;
+  }
+};
+const [addressState, dispatch] = useReducer(addressReducer, initialAddress);
+const handleInputChange = (event) => {
+  return dispatch({
+    type: "UPDATE_INPUT",
+    payload: { name: event.target.name, value: event.target.value },
+  });
+};
+const addNewAddress=(event)=>{
+  const values=Object.values(addressState).join('_');
+  // console.log(values);
+  // console.log(user_id);
+  const addressValue={
+    'user_id':user_id,
+    'address':values
+  }
+  // addressValue
+httpRequest(addressValue,"createAddress.php" ).then(changeviewForm).then(getAddress);
+}
+useEffect(() => {
+  getAddress().then((data) => {
+    setAddressCallBack(data);
+  });
+},[]);
   return(
 <>
 {/* <div className="orderContainer">
@@ -122,10 +170,17 @@ export const Addaddress=()=>{
 <div className="orderDetails">
 
 <div className="flex-child">
-  <input type="text" placeholder="Pincode" className="signInControl" />
+  <input type="number" placeholder="Pincode" className="signInControl"
+    value={addressState.pincode}
+   onChange={handleInputChange}
+   name="pincode"
+  />
 </div>
 <div className="flex-child">
-  <input type="text" placeholder="Locality" className="signInControl" />
+  <input type="text" placeholder="Locality" className="signInControl" 
+  value={addressState.locality}
+  onChange={handleInputChange}
+  name="locality"/>
 </div>
 
 
@@ -136,29 +191,55 @@ export const Addaddress=()=>{
     className="signInControl textArea txt-area"
     placeholder="Enter Your Address(Area and Street)"
     rows="4"
-    name="address"
+    value={addressState.address}
+   onChange={handleInputChange}
+   name="address"
   ></textarea>
 </div>
 </div>
 <div className="orderDetails">
 <div className="flex-child">
-  <input type="text" placeholder="City/District/Town" className="signInControl" />
+  <input type="text" placeholder="City/District/Town"
+  value={addressState.city}
+  onChange={handleInputChange}
+  name="city"
+  className="signInControl" />
 </div>
 <div className="flex-child">
-  <input type="text" placeholder="State" className="signInControl" />
+<input type="number" placeholder="Alternate Phone No (Optional)" className="signInControl" 
+  value={addressState.altphoneno}
+  onChange={handleInputChange}
+  name="altphoneno"
+  />
 </div>
 </div>
+{/* <div className="orderDetails">
+<div className="flex-child">
+  <input type="text" placeholder="Landmark(Optional)"
+  value={addressState.landmark}
+  onChange={handleInputChange}
+  name="landmark"
+  className="signInControl" />
+</div>
+<div className="flex-child">
+  <input type="text" placeholder="Alternate Phone No (Optional)" className="signInControl" 
+  value={addressState.altphoneno}
+  onChange={handleInputChange}
+  name="altphoneno"
+  />
+</div>
+</div> */}
 <div className="orderDetails">
 <div className="flex-child">
-  <input type="text" placeholder="Landmark(Optional)" className="signInControl" />
+<button  className="cancelBtn" onClick={changeviewForm} type="submit">Cancel</button>
 </div>
 <div className="flex-child">
-  <input type="text" placeholder="Alternate Phone No (Optional)" className="signInControl" />
+<button className="saveBtn" type="submit" onClick={addNewAddress}>Save</button>
 </div>
 </div>
-<div className="orderDetails">
-<button className="saveBtn" type="submit">Save</button>
-</div>
+{/* <div className="orderDetails">
+<button  className="cancelBtn" onClick={changeviewForm} type="submit">Cancel</button>
+</div> */}
 
 </div>
       {/* </div> */}
