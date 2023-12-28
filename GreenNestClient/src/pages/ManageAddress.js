3aa98566043230b8ae38a8d37ed939a7a8f7c1d6
11Aravind <aravindsreenandanam11@gmail.com>
2023-12-28 16:34:03 +0530

    manage address componet was updated

--- GreenNestClient/src/pages/ManageAddress.js
+++ GreenNestClient/src/pages/ManageAddress.js
@@ -4,11 +4,11 @@ import { httpRequest } from "../API/api";
 import { useSelector } from "react-redux";
 import "./Order.css";
 
-export const AddressCard = ({address:addressObj, setAddress,changeAddressSelection,isAddressSelectionRequired=false,selectedAddressId  }) => {
+export const AddressCard = ({ address: addressObj, setAddress, changeAddressSelection, isAddressSelectionRequired = false, selectedAddressId }) => {
   const { address, address_id } = addressObj;
   const [PriviousAddressValue, setEditAddress] = useState("");
   const [addressId, AddressIdForUpdation] = useState(-1);
-  const [isEditFormVisible,setIsEditFormVisible]=useState(-1)
+  const [isEditFormVisible, setIsEditFormVisible] = useState(-1)
   const deleteAddress = (event, address_id) => {
     httpRequest({ address_id }, "deleteAddress.php").then(() => {
       getAddress().then((data) => {
@@ -16,23 +16,13 @@ export const AddressCard = ({address:addressObj, setAddress,changeAddressSelecti
       });
     });
   }
-// console.log(isEditFormVisible)
   const editAddress = (event, address_id) => {
-    // httpRequest({ address_id }, "getOneAddress.php").then((data) => {
-    //   var str = data[0].address
-    //   str = str.split("_")
-    //   setEditAddress(str)
-    //   AddressIdForUpdation(address_id)
-    // });
     changeAddressSelection(address_id);
-    // console.log(selectedAddressId)
-    // console.log(isEditFormVisible)
-    setEditAddress( address.split("_"));
+    setEditAddress(address.split("_"));
     setIsEditFormVisible(address_id);
-  
-    // AddressIdForUpdation(address_id)
+    AddressIdForUpdation(address_id)
   }
-
+var addressArray=address.split('_')
   return (
     <>
       <div>
@@ -42,14 +32,15 @@ export const AddressCard = ({address:addressObj, setAddress,changeAddressSelecti
               {isAddressSelectionRequired &&
                 <div>
                   <label htmlFor="addressRadio"></label>
-                  <input type="radio" name="addressRadio" value={address_id} onChange={(event)=> {changeAddressSelection(event.target.value);setIsEditFormVisible(null)}} id="addressRadio" />
-                </div> 
+                  <input type="radio" name="addressRadio" value={address_id} onChange={(event) => { changeAddressSelection(event.target.value); setIsEditFormVisible(null) }} id="addressRadio" />
+                </div>
               }
-              {address_id}
             </div>
             <div className="transaction_id font12">
-              <b><textarea name="" value={address.replace(/_/g, " , ")} id="editAddressBox" cols="50" rows="5" readOnly>
-              </textarea></b>
+              <b>
+                <textarea name="" value={address.replace(/_/g, " , ")} id="editAddressBox" cols="50" rows="5" readOnly>
+              </textarea>
+              </b>
             </div>
             <div className="status font12" >
               <i className="fa fa-pencil-square-o"
@@ -64,8 +55,8 @@ export const AddressCard = ({address:addressObj, setAddress,changeAddressSelecti
 
             </div>
           </div>
-       
-        {selectedAddressId==isEditFormVisible && <EditAddressDetails PriviousAddressValue={PriviousAddressValue} addressId={addressId} setAddress={setAddress} setIsEditFormVisible={setIsEditFormVisible} />}
+
+          {selectedAddressId == isEditFormVisible && <EditAddressDetails PriviousAddressValue={PriviousAddressValue} addressId={addressId} setAddress={setAddress} setIsEditFormVisible={setIsEditFormVisible} />}
         </div>
       </div>
     </>
@@ -77,14 +68,14 @@ export const CreateNewAddress = ({ toggleAddressForm, setAddress }) => {
   const nameRef = useRef(null);
   const addressRef = useRef(null);
   const locationRef = useRef(null);
+  const landmarkRef = useRef(null);
   const cityRef = useRef(null);
+  const stateRef = useRef(null);
   const pincodeRef = useRef(null);
   const alterPhoneNoRef = useRef(null);
-  const landmarkRef = useRef(null);
-  const stateRef = useRef(null);
   const user_id = useSelector((state) => state.user.user_id);
   const addNewAddressHandler = (event) => {
-    var addressValueObject = [nameRef.current.value, addressRef.current.value, locationRef.current.value, cityRef.current.value, pincodeRef.current.value, alterPhoneNoRef.current.value,landmarkRef.current.value,stateRef.current.value]
+    var addressValueObject = [nameRef.current.value, addressRef.current.value, locationRef.current.value, cityRef.current.value, pincodeRef.current.value, alterPhoneNoRef.current.value, landmarkRef.current.value, stateRef.current.value]
     addressValueObject = addressValueObject.join('_')
     const addressValue = {
       'user_id': user_id,
@@ -116,26 +107,26 @@ export const CreateNewAddress = ({ toggleAddressForm, setAddress }) => {
           </div>
         </div>
         <div className="orderDetails">
-        <div className="flex-child">
+          <div className="flex-child">
             <input type="number" placeholder="Pincode *" className="signInControl"
               ref={pincodeRef}
               name="pincode"
             />
           </div>
-        <div className="flex-child">
+          <div className="flex-child">
             <input type="text" placeholder="Locality / Area/ Street *" className="signInControl"
               ref={locationRef}
               name="locality" />
           </div>
         </div>
         <div className="orderDetails">
-        <div className="flex-child">
+          <div className="flex-child">
             <input
               className="signInControl "
               placeholder="Building name / Falt number *"
               ref={addressRef}
               name="address"
-           />
+            />
           </div>
           <div className="flex-child">
             <input type="text" placeholder="City/District *"
@@ -146,13 +137,13 @@ export const CreateNewAddress = ({ toggleAddressForm, setAddress }) => {
         </div>
         {/* new  */}
         <div className="orderDetails">
-        <div className="flex-child">
+          <div className="flex-child">
             <input
               className="signInControl "
               placeholder="Landmark *"
               ref={landmarkRef}
               name="address"
-           />
+            />
           </div>
           <div className="flex-child">
             <input type="text" placeholder="State *"
@@ -173,15 +164,15 @@ export const CreateNewAddress = ({ toggleAddressForm, setAddress }) => {
     </>
   );
 }
-export const EditAddressDetails = ({ PriviousAddressValue, addressId,setAddress,setIsEditFormVisible }) => {
+export const EditAddressDetails = ({ PriviousAddressValue, addressId, setAddress, setIsEditFormVisible }) => {
   const nameRef = useRef();
   const addressRef = useRef();
   const locationRef = useRef();
+  const landmarkRef = useRef();
   const cityRef = useRef();
+  const stateRef = useRef();
   const pincodeRef = useRef();
   const alterPhoneNoRef = useRef();
-  const landmarkRef = useRef(null);
-  const stateRef = useRef(null);
   useEffect(() => {
     nameRef.current.value = PriviousAddressValue[0] || ''
     addressRef.current.value = PriviousAddressValue[1] || ''
@@ -189,26 +180,26 @@ export const EditAddressDetails = ({ PriviousAddressValue, addressId,setAddress,
     cityRef.current.value = PriviousAddressValue[3] || ''
     pincodeRef.current.value = PriviousAddressValue[4] || ''
     alterPhoneNoRef.current.value = PriviousAddressValue[5] || ''
-    landmarkRef.current.value=PriviousAddressValue[6] ||''
-    stateRef.current.value=PriviousAddressValue[7] || ''
+    landmarkRef.current.value = PriviousAddressValue[6] || ''
+    stateRef.current.value = PriviousAddressValue[7] || ''
   }, [PriviousAddressValue]);
 
   const updateAddressHandler = (event) => {
-    var addressValueObject = [nameRef.current.value, addressRef.current.value, locationRef.current.value, cityRef.current.value, pincodeRef.current.value, alterPhoneNoRef.current.value,landmarkRef.current.value,stateRef.current.value]
+    var addressValueObject = [nameRef.current.value, addressRef.current.value, locationRef.current.value, cityRef.current.value, pincodeRef.current.value, alterPhoneNoRef.current.value, landmarkRef.current.value, stateRef.current.value]
     addressValueObject = addressValueObject.join('_')
-    console.log(addressValueObject)
     const addressValue = {
       'address_id': addressId,
       'address': addressValueObject
     }
     httpRequest(addressValue, "updateAddress.php").then(() => getAddress().then((data) => {
-      setAddress(data)
+      setAddress(data);
+      setIsEditFormVisible(false);
     }));
   }
   return (
     <>
-      <div >
-      <div className="orderDetails">
+      <div>
+        <div className="orderDetails">
           <div className="flex-child">
             <input type="text" placeholder="Name *" className="signInControl"
               ref={nameRef}
@@ -223,26 +214,26 @@ export const EditAddressDetails = ({ PriviousAddressValue, addressId,setAddress,
           </div>
         </div>
         <div className="orderDetails">
-        <div className="flex-child">
+          <div className="flex-child">
             <input type="number" placeholder="Pincode *" className="signInControl"
               ref={pincodeRef}
               name="pincode"
             />
           </div>
-        <div className="flex-child">
+          <div className="flex-child">
             <input type="text" placeholder="Locality / Area/ Street *" className="signInControl"
               ref={locationRef}
               name="locality" />
           </div>
         </div>
         <div className="orderDetails">
-        <div className="flex-child">
+          <div className="flex-child">
             <input
               className="signInControl "
               placeholder="Building name / Falt number *"
               ref={addressRef}
               name="address"
-           />
+            />
           </div>
           <div className="flex-child">
             <input type="text" placeholder="City/District *"
@@ -253,13 +244,13 @@ export const EditAddressDetails = ({ PriviousAddressValue, addressId,setAddress,
         </div>
         {/* new  */}
         <div className="orderDetails">
-        <div className="flex-child">
+          <div className="flex-child">
             <input
               className="signInControl "
               placeholder="Landmark *"
               ref={landmarkRef}
               name="address"
-           />
+            />
           </div>
           <div className="flex-child">
             <input type="text" placeholder="State *"
@@ -270,18 +261,18 @@ export const EditAddressDetails = ({ PriviousAddressValue, addressId,setAddress,
         </div>
         <div className="orderDetails">
           <div className="flex-child">
-            <button className="cancelBtn" type="submit" onClick={()=>{setIsEditFormVisible(false)}}>Cancel</button>
+            <button className="cancelBtn" type="submit" onClick={() => { setIsEditFormVisible(false) }}>Cancel</button>
           </div>
           <div className="flex-child">
             <button className="saveBtn" type="submit" onClick={updateAddressHandler}>Update</button>
           </div>
         </div>
-       </div>
+      </div>
     </>
   );
 }
 
-const ManageAddress = ({changeAddressSelection, isAddressSelectionRequired ,selectedAddressId}) => {
+const ManageAddress = ({ changeAddressSelection, isAddressSelectionRequired, selectedAddressId }) => {
   const [addresses, setAddress] = useState([]);
   const [showAddressForm, toggleAddressForm] = useState(false);
   useEffect(() => {
@@ -298,20 +289,20 @@ const ManageAddress = ({changeAddressSelection, isAddressSelectionRequired ,sele
               {showAddressForm ? ("") : (<span className="pointer"> <i className="fa fa-plus" aria-hidden="true" ></i>ADD A NEW ADDRESS</span>)}
             </b>
           </div>
-          {showAddressForm && <CreateNewAddress toggleAddressForm={toggleAddressForm} setAddress={setAddress} /> }
+          {showAddressForm && <CreateNewAddress toggleAddressForm={toggleAddressForm} setAddress={setAddress} />}
         </div>
       </div>
       {addresses.map((address, index) => {
         return (
 
-            <AddressCard
-              key={index}
-              address={address}
-              setAddress={setAddress}
-              changeAddressSelection={changeAddressSelection}
-              isAddressSelectionRequired={isAddressSelectionRequired}
-              selectedAddressId={selectedAddressId}
-            />
+          <AddressCard
+            key={index}
+            address={address}
+            setAddress={setAddress}
+            changeAddressSelection={changeAddressSelection}
+            isAddressSelectionRequired={isAddressSelectionRequired}
+            selectedAddressId={selectedAddressId}
+          />
         );
       }
       )}
